cfd74ad76fe11081a238573ed62a59290c443b5a
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-08-23 21:54:45 +0300

    - Modellerin oluşturulması işlemleri tamamlandı.

---
+++ Order.Api/Models/Order.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Order.Api.Models
+{
+    public class Order
+    {
+        public Order()
+        {
+            OrderItems = new List<OrderItem>();
+        }
+
+        public int Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public int BuyerId { get; set; }
+        public ICollection<OrderItem> OrderItems { get; set; }
+        public Status Status { get; set; }
+        public string Fail { get; set; }
+        public Address Address { get; set; }
+    }
+
+    public enum Status
+    {
+        Suspend,
+        Success,
+        Fail
+    }
+}
