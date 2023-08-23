26d80050389a549f6642530d9bdb09da4b18c138
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-08-23 22:33:23 +0300

    - OrderItem message oluşturuldu. - OrderCreatedEvent'ı oluşturuldu. - PaymentMessage oluşturuldu.

---
+++ Shared/OrderEvents/OrderCreatedEvent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared.Messages;
+
+namespace Shared.OrderEvents
+{
+    public class OrderCreatedEvent
+    {
+        public OrderCreatedEvent()
+        {
+            OrderItemMessages = new List<OrderItemMessage>();
+        }
+        public int OrderId { get; set; }
+        public string BuyerId { get; set; }
+        public PaymentMessage Payment { get; set; }
+        public List<OrderItemMessage> OrderItemMessages { get; set; }
+    }
+}
