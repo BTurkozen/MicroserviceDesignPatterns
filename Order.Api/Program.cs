6fa92eebfc3a64234c691ff690151107d008df76
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-08-23 22:07:56 +0300

    - DataContext oluşturuldu. - appsetting içerisine db path verildi. - program.cs içerisine conectionstring tanımlandı.

--- Order.Api/Program.cs
+++ Order.Api/Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Order.Api.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +12,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionStr"));
+});
 
 var app = builder.Build();
 
