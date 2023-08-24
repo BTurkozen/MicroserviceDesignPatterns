80f7a0d34fc4f5e435f98c4c50ffffb15257ea9a
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-08-24 22:05:54 +0300

    - Order Controller oluşturuldu. - Order ile ilgili Dto'larımızı oluşturduk.

---
+++ Order.Api/Dtos/OrderItemDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Order.Api.Dtos
+{
+    public class OrderItemDto
+    {
+        public int ProductId { get; set; }
+        public int Count { get; set; }
+        public decimal Price { get; set; }
+    }
+}
