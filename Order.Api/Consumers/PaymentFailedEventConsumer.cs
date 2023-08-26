cc95a186177de7c75e430a462d9415a7c61fb3da
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-08-26 17:49:50 +0300

    - paymentfailed consumer içeriği dolduruldu.

--- Order.Api/Consumers/PaymentFailedEventConsumer.cs
+++ Order.Api/Consumers/PaymentFailedEventConsumer.cs
@@ -1,4 +1,6 @@
 ﻿using MassTransit;
+using Microsoft.Extensions.Logging;
+using Order.Api.Models;
 using Shared.PaymentEvent;
 using System.Threading.Tasks;
 
@@ -6,9 +8,31 @@ namespace Order.Api.Consumers
 {
     public class PaymentFailedEventConsumer : IConsumer<PaymentFailedEvent>
     {
-        public Task Consume(ConsumeContext<PaymentFailedEvent> context)
+        private readonly DataContext _dataContext;
+        private readonly ILogger<PaymentFailedEventConsumer> _logger;
+
+        public PaymentFailedEventConsumer(DataContext dataContext, ILogger<PaymentFailedEventConsumer> logger)
         {
-            throw new System.NotImplementedException();
+            _dataContext = dataContext;
+            _logger = logger;
+        }
+
+        public async Task Consume(ConsumeContext<PaymentFailedEvent> context)
+        {
+            var order = await _dataContext.Orders.FindAsync(context.Message.OrderId);
+
+            if (order is not null)
+            {
+                order.Status = Status.Fail;
+                order.Fail = context.Message.Message;
+                await _dataContext.SaveChangesAsync();
+
+                _logger.LogInformation($"OrderId: {context.Message.OrderId} status changed: {order.Status}");
+            }
+            else
+            {
+                _logger.LogInformation($"OrderId: {context.Message.OrderId} not found");
+            }
         }
     }
 }
