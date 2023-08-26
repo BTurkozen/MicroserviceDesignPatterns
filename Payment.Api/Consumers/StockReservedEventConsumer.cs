8096db0762fe6ffd8ec348dde71d9cc3dca1e72e
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-08-26 16:25:44 +0300

    - Payment api consumer işlemleri tamamlandı. - proje .net7 olarak yükseltildi startup içerisi düzenlendi ve  masstransit kuyruk işlemleriş eklendi.

---
+++ Payment.Api/Consumers/StockReservedEventConsumer.cs
@@ -0,0 +1,47 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Logging;
+using Shared.PaymentEvent;
+using Shared.StockEvents;
+using System.Threading.Tasks;
+
+namespace Payment.Api.Consumers
+{
+    public class StockReservedEventConsumer : IConsumer<StockReservedEvent>
+    {
+        private readonly ILogger<StockReservedEventConsumer> _logger;
+        private readonly IPublishEndpoint _publishEndpoint;
+
+        public StockReservedEventConsumer(ILogger<StockReservedEventConsumer> logger, IPublishEndpoint publishEndpoint)
+        {
+            _logger = logger;
+            _publishEndpoint = publishEndpoint;
+        }
+
+        public async Task Consume(ConsumeContext<StockReservedEvent> context)
+        {
+            var balance = 3000m;
+
+            if (balance > context.Message.PaymentMessage.TotalPrice)
+            {
+                _logger.LogInformation($"{context.Message.PaymentMessage.TotalPrice}₺ was withrawn from credit card for userId: {context.Message.BuyerId}");
+
+                await _publishEndpoint.Publish(new PaymentSuccessedEvent
+                {
+                    OrderId = context.Message.OrderId,
+                    BuyerId = context.Message.BuyerId,
+                });
+            }
+            else
+            {
+                _logger.LogInformation($"{context.Message.PaymentMessage.TotalPrice}₺ was not withrawn from credit card for userId: {context.Message.BuyerId}");
+
+                await _publishEndpoint.Publish(new PaymentFailedEvent
+                {
+                    BuyerId = context.Message.BuyerId,
+                    OrderId = context.Message.OrderId,
+                    Message = "Not enough balance"
+                });
+            }
+        }
+    }
+}
--- Payment.Api/Program.cs
+++ Payment.Api/Program.cs
@@ -1,26 +1,49 @@
-using Microsoft.AspNetCore.Hosting;
+using MassTransit;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Payment.Api.Consumers;
+using Shared.Settings;
 
-namespace Payment.Api
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddMassTransit(options =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    options.AddConsumer<StockReservedEventConsumer>();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    options.UsingRabbitMq((context, config) =>
+    {
+        config.Host(builder.Configuration.GetConnectionString("RabbitMq"));
+
+        config.ReceiveEndpoint(RabbitMqSettingsConst.StockReservedEventQueueName, e =>
+        {
+            e.ConfigureConsumer<StockReservedEventConsumer>(context);
+        });
+    });
+});
+
+var logger = builder.Services.BuildServiceProvider().GetService<ILogger<StockReservedEventConsumer>>();
+
+builder.Services.AddSingleton(typeof(ILogger), logger);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
