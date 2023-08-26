0f84c42819cfb5d3e20cc9a3e41d684eec520498
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-08-26 18:00:04 +0300

    - StockNotreservedEventConsumer içeriği dolduruldu.

--- Order.Api/Consumers/StockNotreservedEventConsumer.cs
+++ Order.Api/Consumers/StockNotreservedEventConsumer.cs
@@ -1,5 +1,7 @@
 ﻿using MassTransit;
+using Microsoft.Extensions.Logging;
 using Order.Api.Models;
+using Shared.PaymentEvent;
 using Shared.StockEvents;
 using System.Threading.Tasks;
 
@@ -8,10 +10,30 @@ namespace Order.Api.Consumers
     public class StockNotreservedEventConsumer : IConsumer<StockNotReservedEvent>
     {
         private readonly DataContext _dataContext;
+        private readonly ILogger<StockNotreservedEventConsumer> _logger;
 
-        public Task Consume(ConsumeContext<StockNotReservedEvent> context)
+        public StockNotreservedEventConsumer(DataContext dataContext, ILogger<StockNotreservedEventConsumer> logger)
         {
-            throw new System.NotImplementedException();
+            _dataContext = dataContext;
+            _logger = logger;
+        }
+
+        public async Task Consume(ConsumeContext<StockNotReservedEvent> context)
+        {
+            var order = await _dataContext.Orders.FindAsync(context.Message.OrderId);
+
+            if (order is not null)
+            {
+                order.Status = Status.Fail;
+                order.Fail = context.Message.Message;
+                await _dataContext.SaveChangesAsync();
+
+                _logger.LogInformation($"OrderId: {context.Message.OrderId} status changed: {order.Status}");
+            }
+            else
+            {
+                _logger.LogInformation($"OrderId: {context.Message.OrderId} not found");
+            }
         }
     }
 }
